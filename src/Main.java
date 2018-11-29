d373c7cca26fcd7f5bc613fd5f4a47d8a5d39b97
BasemaHossian <basemayemen@yahoo.com>
2018-11-29 15:30:24 +0200

    add countPrime function

--- src/Main.java
+++ src/Main.java
@@ -298,7 +298,25 @@ public class Main {
 
     //ID 20160316
     int countPrimes(int[] a){
-        return 0;
+        int counter = 0;
+        boolean flag = true;
+        for (int i = 0; i < a.length; i++) {
+            flag = true;
+            int n = a[i];
+            for (int j = 2; j <= n / 2; ++j) {
+                if (n % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if ((n == 0) || (n == 1)) {
+                flag = false;
+            }
+            if (flag) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     //ID 20160157
